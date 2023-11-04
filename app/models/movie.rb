ffe524f85ca8ccc572dd49c25c4ac57616a55acd
vitaliit-vvd <vitalii.t@vevidi.com>
2023-11-04 12:54:12 +0200

    add autocomplete search to movies

---
+++ app/models/movie.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+class Movie < ApplicationRecord
+  validates :title, presence: true, uniqueness: true
+
+  scope :filter_by_title, ->(title) { where('title ILIKE ?', "%#{title}%") }
+end
---
+++ app/views/movies/_movie.json.jbuilder
@@ -0,0 +1,4 @@
+# frozen_string_literal: true
+
+json.extract! movie, :id, :title, :created_at, :updated_at
+json.url movie_url(movie, format: :json)
